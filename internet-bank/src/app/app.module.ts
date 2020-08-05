fc098be0fea51113f656b1e3ee94c6278f957004
Mario <mario@MacOSs-MacBook-Pro.local>
2020-08-05 20:04:16 -0300

    iniciando projeto internet bank criando

---
+++ internet-bank/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, ContentComponent, FooterComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatDialogModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
