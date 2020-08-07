d6261c3ec8e61f22ed0d0fc6c1d9bfeaf885a937
Mario <mario@MacOSs-MacBook-Pro.local>
2020-08-07 19:36:06 -0300

    inserindo rotas

--- internet-bank/src/app/app.module.ts
+++ internet-bank/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ContentComponent, FooterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatDialogModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
